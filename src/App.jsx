cc4387086ef2f2c377c32d0be7f1859889250069
P.K Selani Didualni <pkselani00@gmail.com>
2024-02-07 17:34:18 -0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
